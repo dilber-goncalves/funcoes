860a44f3a2d52bd38a8d4967313d3b23440485fa
Dilber Goncalves <dilberkeybd@gmail.com>
2024-04-01 15:30:17 -0300

    01-04

---
+++ primeiras-funcoes.js
@@ -0,0 +1,13 @@
+/*function imprimirOla (){
+    console.log("Olá");
+}
+
+imprimirOla();*/
+
+function cumprimentar (pessoa, idade){
+    //const pessoa = "José";
+    console.log(`Olá, ${pessoa}! Fiquei sabendo que vc tem ${idade} anos.`);
+}
+
+cumprimentar("José", 30);
+cumprimentar("Dilber", 28);
